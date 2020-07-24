d45efef950ede61b24d1f4e26d5b95993f02462d
johnna_endure <cwooseok60@gmail.com>
2020-07-25 01:15:20 +0900

    @EnableAspectJAutoProxy 추가

--- src/main/java/com/springboot/myshop/MyshopApplication.java
+++ src/main/java/com/springboot/myshop/MyshopApplication.java
@@ -9,8 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class MyshopApplication {
 
     public static void main(String[] args) {
